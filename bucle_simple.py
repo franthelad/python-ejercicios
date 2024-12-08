98c40fe0c6ca9cc13c59eeed4a05faf9d3d45b54
franthelad <120594638+franthelad@users.noreply.github.com>
2024-12-08 11:32:33 +0100

    Create bucle_simple.py

---
+++ bucle_simple.py
@@ -0,0 +1,18 @@
+# Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
+
+sample = input('por vafor, intorduce una palabra: ')
+
+print('\nBUCLE FOR\n')
+
+for i in range(10):
+
+    print(sample)
+
+print('\nBUCLE FOR\n')
+
+x = 0
+
+while x < 10:
+
+    print(sample)
+    x += 1
